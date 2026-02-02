b589ea47b5fc9396394692977eac11967a5f6edf
kcadet-dev <kevincadet422@gmail.com>
2026-02-02 12:45:18 -0500

    Max Function Completed

--- index.js
+++ index.js
@@ -21,7 +21,19 @@ function min(arr) {
   return min;
 }
 
+function max(arr) {
+  let max = arr[0];
+
+  for (const n of arr) {
+    if (n > max) {
+      max = n;
+    }
+  }
+  return max;
+}
+
 // Test Cases
 console.log("Sum:" + sum([1,2,3]))
 console.log("Avg:" + average([1,2,3]))
 console.log("Min:" + min([1,2,3]))
+console.log("Max:" + max([1,2,3]))
