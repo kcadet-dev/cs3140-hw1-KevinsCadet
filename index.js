d831cddc7131f47b35de95cee7eecba6ee391577
kcadet-dev <kevincadet422@gmail.com>
2026-02-02 12:58:00 -0500

    Vowel Function Completed

--- index.js
+++ index.js
@@ -33,13 +33,27 @@ function max(arr) {
 }
 
 function capitalize(str) {
-  if (str.length === 1) {
+  if (str.length == 1) {
     return str.toUpperCase();
   }
 
   else {
   return str.charAt(0).toUpperCase() + str.slice(1);
   }
+}
+
+function countVowels(str) {
+ vowels = ['a', 'e', 'i', 'o', 'u'];
+ let count = 0;
+
+ for (const char of str.toLowerCase()) {
+  if (vowels.includes(char)) {
+    count += 1;
+    }
+ }
+
+ return count;   
+
 }
 
 // Test Cases
@@ -49,3 +63,5 @@ console.log("Min:" + min([1,2,3]))
 console.log("Max:" + max([1,2,3]))
 console.log("Cap:" + capitalize("hello"))
 console.log("Cap:" + capitalize("w"))
+console.log("Vowels:" + countVowels("hello"))   
+console.log("Vowels:" + countVowels("gym"))   
