f65b8309f82d334c350e96fb0dda01662b01681a
kcadet-dev <kevincadet422@gmail.com>
2026-02-02 13:11:01 -0500

    Object Completed

--- index.js
+++ index.js
@@ -56,32 +56,44 @@ function countVowels(str) {
 
 }
 
-function countVowels(str) {
- vowels = ['a', 'e', 'i', 'o', 'u'];
- let count = 0;
-
- for (const char of str.toLowerCase()) {
-  if (vowels.includes(char)) {
-    count += 1;
-    }
- }
-
- return count;   
-
-}
-
 function reverse(str) {
   return str.split('').reverse().join('');
 }
 
-// Test Cases
+const student = {
+
+name: "Kevins Cadet",
+age: 20,
+grades: [85, 90, 78],
+
+getAverage: function() {
+    return average(this.grades);
+},
+
+isHonorRoll: function() {
+    return this.getAverage() >= 85;
+}
+
+}
+
+// Array Functions Text Cases
 console.log("Sum:" + sum([1,2,3]))
 console.log("Avg:" + average([1,2,3]))
 console.log("Min:" + min([1,2,3]))
 console.log("Max:" + max([1,2,3]))
+
+// String Functions Test Cases
 console.log("Cap:" + capitalize("hello"))
 console.log("Cap:" + capitalize("w"))
 console.log("Vowels:" + countVowels("hello"))   
 console.log("Vowels:" + countVowels("gym"))
 console.log("Reverse:" + reverse("hello"))
-console.log("Reverse:" + reverse("w"))   
+console.log("Reverse:" + reverse("w"))
+
+// Object Test Cases
+console.log("Student Name:" + student.name)    
+console.log("Student Age:" + student.age)
+console.log("Student Grades:" + student.grades)
+console.log("Student Average:" + student.getAverage())
+console.log("Is Honor Roll:" + student.isHonorRoll())
+console.log(student);
